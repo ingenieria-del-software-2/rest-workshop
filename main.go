a8c9b5d727f6f5f940b0855adc3ff4e9c5c07a84
ignacio lugones <ignacio.lugones@wibson.org>
2024-08-22 00:49:31 -0300

    remove unnecessary code

--- main.go
+++ main.go
@@ -4,16 +4,6 @@ import (
 	"rest-workshop/src/router"
 )
 
-type Item struct {
-	Id    string  `json:"id"`
-	Name  string  `json:"name"`
-	Price float64 `json:"price"`
-}
-
-type Error struct {
-	Message string `json:"message"`
-}
-
 func main() {
 	r := router.CreateRouter()
 	if err := r.Run(); err != nil {
