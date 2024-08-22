62def918fca00d5854effda53da0ed56d1e58632
ignacio lugones <ignacio.lugones@wibson.org>
2024-08-21 22:28:12 -0300

    add data for the package by layer feature

---
+++ src/router/main.go
@@ -0,0 +1,11 @@
+package router
+
+type Item struct {
+	Id    string  `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
