272ece05115eac7c5e9726bd67d0c5f363eec7b2
ignacio lugones <ignacio.lugones@wibson.org>
2024-08-21 23:41:03 -0300

    add router code

--- src/router/main.go
+++ src/router/main.go
@@ -2,9 +2,16 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"rest-workshop/src/controller"
+	"rest-workshop/src/database"
+	"rest-workshop/src/models"
+	"rest-workshop/src/service"
 )
 
 func CreateRouter() *gin.Engine {
 	r := gin.Default()
+	db := database.CreateDB[models.Item]()
+	c := controller.CreateControllerItem(service.CreateServiceItem(db))
+	r.POST("/item", c.AddItem)
 	return r
 }
