e2c12c4fb35362d5165b3d1baf0e1988858b266a
ignacio lugones <ignacio.lugones@wibson.org>
2024-08-21 22:11:10 -0300

    definir correctamente el endpoint

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Item struct {
+	Id    string  `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
 
 func main() {
 	r := gin.Default()
+	database := make(map[string]Item, 0)
 	r.POST("/item", func(context *gin.Context) {
-		context.JSON(200, struct{}{})
+		var item Item
+		if err := context.Bind(&item); err != nil {
+			context.JSON(http.StatusBadRequest, Error{err.Error()})
+			return
+		} else if item.Id == "" {
+			context.JSON(http.StatusBadRequest, Error{"missing id"})
+			return
+		}
+		if _, found := database[item.Id]; found {
+			context.JSON(http.StatusConflict, Error{"already existed"})
+			return
+		}
+		database[item.Id] = item
+		context.JSON(201, item)
 	})
 	if err := r.Run(); err != nil {
 		panic(err.Error())
