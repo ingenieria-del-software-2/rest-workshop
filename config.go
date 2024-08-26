bcca110be14f9c8d5a6d1a4810b4827c221d1662
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2024-08-26 17:50:05 -0300

    feat: config

---
+++ config.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+// Config structure to hold the environment variables.
+type Config struct {
+	Host             string
+	Port             string
+	Environment      string
+	DatabaseHost     string
+	DatabasePort     string
+	DatabaseUser     string
+	DatabasePassword string
+}
+
+// LoadConfig loads environment variables using Viper and returns a Config structure.
+func LoadConfig() *Config {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv() // Reads environment variables
+
+	// Default environment variables
+	viper.SetDefault("PORT", "8080")
+	viper.SetDefault("HOST", "0.0.0.0")
+	viper.SetDefault("ENVIRONMENT", "development")
+
+	// Load the .env file if it exists
+	if err := viper.ReadInConfig(); err != nil {
+		log.Printf("The .env configuration file was not found or there was an error reading it: %v", err)
+	}
+
+	config := &Config{
+		Host:             viper.GetString("HOST"),
+		Port:             viper.GetString("PORT"),
+		Environment:      viper.GetString("ENVIRONMENT"),
+		DatabaseHost:     viper.GetString("DATABASE_HOST"),
+		DatabasePort:     viper.GetString("DATABASE_PORT"),
+		DatabaseUser:     viper.GetString("DATABASE_USER"),
+		DatabasePassword: viper.GetString("DATABASE_PASSWORD"),
+	}
+
+	return config
+}
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"rest-workshop/src/router"
 )
 
 func main() {
+	config := LoadConfig()
+
 	r := router.CreateRouter()
-	if err := r.Run(); err != nil {
+
+	address := fmt.Sprintf("%s:%s", config.Host, config.Port)
+	if err := r.Run(address); err != nil {
 		panic(err.Error())
 	}
 }
